0e93d633b755b0ad33806e1d8bd926df493ba4c0
OthSay <34811599+othsay@users.noreply.github.com>
2020-03-14 21:19:00 +0100

    add main function

---
+++ main.py
@@ -0,0 +1,35 @@
+import click
+import json
+from src.twitter_analyzer import TweetsAnalyzer
+
+
+@click.command()
+@click.argument("config_path")
+@click.argument("query")
+@click.option("--count", default=500, help="number of last tweets in the past to analyze")
+@click.option("--lang", default="en", help="Language of tweets to analyze")
+def main(query, config_path, count, lang):
+    """
+    Function that returns a json with sentiment analysis summary for a query
+    :param query: str :
+        query looking to analyze
+    :param config_path:
+        config path for analysis models
+    :return:
+        result : json
+            json of sentiment analysis
+    """
+
+    conf = json.load(open(config_path, "r"))
+
+    analyzer = TweetsAnalyzer(config=conf)
+
+    df = analyzer.analyze(query=query,
+                          count=count,
+                          lang=lang)
+
+    print(df["BERT_sentiment"].value_counts())
+
+
+if __name__ == '__main__':
+    main()
